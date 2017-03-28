a336c45786c4fc0684a37e4d5a86db82936771c3
naresh.e2011@gmail.com <naresh.e2011@gmail.com>
2017-03-28 17:49:47 -0500

    added trycatch class

---
+++ src/package2/TryCatch.java
@@ -0,0 +1,21 @@
+package package2;
+
+public class TryCatch {
+
+	public static void main(String[] args) {
+		int a= 10;
+		int b = 0,c;
+		try{		// try
+			c= a/b;
+			System.out.println(c);
+		}
+		catch(Exception e){ // catching when it fails 
+			System.out.println("RunTime error cannot divide by zero");
+		}
+		finally {
+			System.out.println("finally block");
+		}
+
+	}
+
+}
