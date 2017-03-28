793101a7ad7bd805801edd0393cd44c5f6373b83
naresh.e2011@gmail.com <naresh.e2011@gmail.com>
2017-03-28 18:15:30 -0500

    added final class

---
+++ src/package2/FinalClass.java
@@ -0,0 +1,13 @@
+package package2;
+
+public class FinalClass {
+
+
+final static double pi = 3.141;
+
+public static void main(String[] args) {
+	int radius = 4;
+	System.out.println("Area of circle: "+(pi*radius*radius));
+}
+
+}
