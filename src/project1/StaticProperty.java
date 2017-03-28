35b732d7dc819b7a31e2b324ec8c0a0785436955
naresh.e2011@gmail.com <naresh.e2011@gmail.com>
2017-03-28 15:11:42 -0500

    added staticproperty class

---
+++ src/project1/StaticProperty.java
@@ -0,0 +1,12 @@
+package project1;
+
+public class StaticProperty {
+
+	static String name1="This is a Static Property";
+	
+	public static void main(String[] args) {
+		System.out.println(name1); // no need to create an object as the property is static
+		
+	}
+
+}
