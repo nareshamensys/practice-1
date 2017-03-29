98106a44df7c5ac1bbd5d367fe3a0d13e15949f7
naresh.e2011@gmail.com <naresh.e2011@gmail.com>
2017-03-29 11:28:04 -0500

    added Abstract Class

---
+++ src/CreatingAbstract/AbstractClass.java
@@ -0,0 +1,12 @@
+package CreatingAbstract;
+
+public abstract class AbstractClass {
+
+	void car(){
+		System.out.println("Name of the car: Volvo");
+	}
+	
+	public abstract void CarProperties(); // abstract method
+ 
+	
+}
---
+++ src/CreatingAbstract/ExtendingAbstract.java
@@ -0,0 +1,12 @@
+package CreatingAbstract;
+
+	
+	public class ExtendingAbstract extends AbstractClass{
+
+		@Override
+		public void CarProperties() {
+			System.out.println(" implementing the un implemented ");
+			
+		}
+		
+	}
